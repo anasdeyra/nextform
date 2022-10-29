fd74eacb5dacde195bf2f1e038fd1ad5524d4d6c
Anass <45261478+anasdeyra@users.noreply.github.com>
2022-10-30 00:16:16 +0100

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,35 @@
-import '../styles/globals.css'
+import Head from "next/head";
+import { MantineProvider } from "@mantine/core";
+import { Global } from "@emotion/react";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App(props) {
+  const { Component, pageProps } = props;
+
+  return (
+    <>
+      <Head>
+        <title>Page title</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          fontFamily: "Poppins, sans-serif",
+          headings: { fontFamily: "Poppins, sans-serif" },
+        }}
+      >
+        <Global
+          styles={[
+            `@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap");`,
+          ]}
+        />
+        <Component {...pageProps} />
+      </MantineProvider>
+    </>
+  );
 }
-
-export default MyApp
